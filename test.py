e4780b17af65e1e38964e37d391f4d06b1a4827f
vdanh <vdanhvt2000@gmail.com>
2023-10-24 18:28:37 +0700

    make changes

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-from measureunit.units import Length
+import logging
+logging.basicConfig(level=logging.INFO, 
+                    format=' %(asctime)s | <%(levelname)s> | %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S')
 
-value = Length.km2m(8)
-value
+from measureunit.units import Length, WeightMass, Temperature
+
+l1 = Length.km2m()
+print(l1)
+
+w1 = WeightMass.t2kg(2)
+print(w1)
+
+t1 = Temperature.C2F(2)
+print(t1)
