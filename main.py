afbcd5183efaafdfc22350a777f5ac154f2cb1f9
vdanh <vdanhvt2000@gmail.com>
2023-11-19 15:34:47 +0700

    make some changes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from measureunit.units import Length
-# import measureunit as msu
+import measureunit as msu
 import logging
 
-# msu.__version__
+print(msu.__version__)
 
 v = Length.km2m(1)
 logging.info(f"The conversion value of 'km2m(1)' is {v}")
